e372fef042cee79f5f5b283078376904b0cbfd05
zkchen <zkchen@zkchen-pc.local>
2016-10-10 23:15:51 +0800

    :grin: 10.10

--- alg_datastructure/clrs/quicksort.py
+++ alg_datastructure/clrs/quicksort.py
@@ -1,24 +1,21 @@
 def partition(A, p, r):
-    key = A[r]    
-    j = p
-    for i in range(p, r, 1):
-        if A[i] < key:
-            A[i], A[j] = A[j], A[i] 
-            j += 1
-    A[j], A[r] = A[r], A[j]
-    return j
+    i = p
+    x = A[r]
+    for j in xrange(p, r):
+        if A[j] <= x:
+            A[i], A[j] = A[j], A[i]
+            i += 1
+    A[r], A[i] = A[i], A[r]
+    return i
 
-
-def quicksort(A, p, r):
-    if p < r:
-        q = partition(A, p, r)
-        quicksort(A, p, q - 1)
-        quicksort(A, q + 1, r)
+def quick_sort(A, p, r):
+    if r > p:
+        i = partition(A, p, r)
+        quick_sort(A, p, i-1)
+        quick_sort(A, i + 1, r)
 
 
 if __name__ == "__main__":
-    import random as rnd
-    A = [rnd.randint(1, 100) for i in xrange(200)]
-    print "before:", A
-    quicksort(A, 0, len(A) - 1)
-    print "after:", A
+    seq = [3,8,4,5,7,1]
+    quick_sort(seq, 0, len(seq) - 1)
+    print seq
