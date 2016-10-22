09783756751e6d5d23508d1ff66cce6f5521b34a
zkchen <zhongkunchen@126.com>
2016-10-22 14:35:31 +0800

    go

--- training/golang/typeconvert.go
+++ training/golang/typeconvert.go
@@ -1,3 +1,8 @@
+// 类型标记：
+// 1.声明、设置变量时如左边未指定类型则编译器根据右值推断类型，如左边指定则检查左右类型；最后都会给变量设置类型标识
+// 2.强制类型转换如果成功则改变类型标识
+// 3.类型断言其实就是判定是否是某个类型标识
+// 4.不能把[]interface{} 转 []int 尽管该interface{}是可以转为int,因为类型标识不兼容
 package main
 
 import (
@@ -51,20 +56,50 @@ func convertBasicType() {
 func convertSlice() {
 	type VSlice []interface{}
 	type ISlice []int
-	var v = VSlice{2, 3}
-	var i = ISlice(v)
-	fmt.Println("convert v to ISlice:", i)
+	//var v = VSlice{2, 3}
+	//var i = ISlice(v)
+	//fmt.Println("convert v to ISlice:", i)
 }
 
 func convertFunc() {
 	type MapFunc func(x interface{})
-	var f = func(x interface{}) {}
-	fmt.Println("f is MapFunc ", MapFunc(f))
-	var f2 = func(x int) {}
-	fmt.Println("f2 is MapFunc ", MapFunc(f2))
+
+	// 在定义变量的时候相当于编译器需要给其打上类型标记，标记设为`func(x interface{})`
+	// 所以断言失败
+	var f interface{} = func(x interface{}) {}
+	if _, ok := f.(MapFunc); ok {
+		fmt.Println("assert f func(x interface{}){} is MapFunc")
+	} else {
+		fmt.Println("assert f func(x interface{}){} isn't MapFunc")
+	}
+
+	// 在定义变量的时候相当于编译器需要给其打上类型标记，此处强制类型转换把标记设为`MapFun`
+	// 所以断言成功
+	var f2 interface{} = MapFunc(func(x interface{}) {})
+	if _, ok := f2.(MapFunc); ok {
+		fmt.Println("assert f2 MapFunc(func(x interface{}){}) is MapFunc")
+	} else {
+		fmt.Println("assert f2 MapFunc(func(x interface{}){}) isn't MapFunc")
+	}
 }
+
+type Sayer interface {
+	Say()
+}
+type Person struct {
+	Name string
+}
+
+func (p *Person) Say() {}
+
+type PT Person
 
 func main() {
 	//convertSlice()
 	convertFunc()
+	var p = &PT{"akun"}
+	p.Say()
+	fmt.Println(p)
+	//p.Say()
+	//(*Person)(p).Say()
 }
--- training/golang/typedef.go
+++ training/golang/typedef.go
@@ -1,10 +1,13 @@
-// 1.函数能作为receiver
+// 1.函数能作为receiver,可以实现接口
 // 2.interface 不能作为receiver,即使重新定义了一个type 自interface
+// 3. type NewType OldType 定义新Type后，不能再用OldType的方法，还可以用旧Type的域;
+//    如果要使用旧Type的方法，应该组合该OldType
 
 package main
 
 import (
 	"fmt"
+	"reflect"
 )
 
 type Runner interface {
@@ -17,7 +20,31 @@ func (p Fn) Run() {
 	fmt.Println("func as receiver and p is ", p)
 }
 
-func main() {
+func testFnAsInterface() {
 	var f interface{} = Fn(func() {})
 	f.(Runner).Run()
+}
+
+type Base struct {
+	Data int
+}
+
+func (p *Base) SayP() {}
+func (p Base) SayV()  {}
+
+type MyValue Base
+
+type MyPointer *Base
+
+// 不能再用原来type的方法，但是fields还可以用
+func testNewTypeMethods() {
+	var mv = MyValue{99}
+	fmt.Println("MyValue.Data:", mv.Data)
+	fmt.Println("MyValue methods cnt:", reflect.ValueOf(mv).NumMethod())
+	var mp = &MyValue{}
+	fmt.Println("MyValue methods cnt:", reflect.ValueOf(mp).NumMethod())
+}
+
+func main() {
+	testNewTypeMethods()
 }
