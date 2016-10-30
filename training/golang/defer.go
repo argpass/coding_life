8f6f439232d21798570a495978640706c3b748d8
zkchen <zhongkunchen@126.com>
2016-10-30 22:41:04 +0800

    defer

--- training/golang/defer.go
+++ training/golang/defer.go
@@ -1,6 +1,7 @@
 // 1.panic 后能触发defer
 // 2.os.Exit 立即退出进程，不会出发defer
 // 3.defer 顺序是倒序(包括defer 中的内嵌defer)
+// 4.有名返回值,返回值可以在defer中得到修改; 使用defer可以在defer中观察函数的返回值，甚至修改返回值(针对有名返回值的情况)
 package main
 
 import (
@@ -40,7 +41,32 @@ func testDeferOnPanic() {
 	panic("panic exit")
 }
 
+func deferReturn() {
+	// 普通返回
+	fn := func() int {
+		defer func() int {
+			// defer 的返回不会影响函数的返回
+			return 9
+		}()
+		return 8
+	}
+	i := fn()
+	fmt.Println("got return ", i)
+
+	// 有名返回值,返回值可以在defer中得到修改
+	fn = func() (r int) {
+		defer func() int {
+			r++
+			return 9
+		}()
+		return r
+	}
+	i = fn()
+	fmt.Println("#2 got return ", i)
+}
+
 func main() {
+	deferReturn()
 	testDeferOrder()
 	testDeferOnPanic()
 	testDeferOnExit()
