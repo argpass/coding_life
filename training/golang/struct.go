3dd0731761cf52903c607f7557d5807d06b64f7f
zkchen <zhongkunchen@126.com>
2016-11-09 13:28:31 +0800

    struct

--- training/golang/struct.go
+++ training/golang/struct.go
@@ -14,12 +14,31 @@ type VChild struct {
 	Parent
 }
 
+type dict map[string]interface{}
+
+type MyDict struct {
+	dict
+}
+
+func (m MyDict) Add(key string, value interface{}) {
+	m.dict[key] = value
+}
+
+func (m MyDict) Show() {
+	fmt.Printf("\nMydict:%v\n", m.dict)
+}
+
+func testMyDict() {
+	var d = MyDict{dict{}}
+	d.Add("name", "akun")
+	d.Add("age", 99)
+	d.Show()
+}
+
 func foo(pp *Parent) {
 	fmt.Println(pp)
 }
 
 func main() {
-	var pc = &Child{}
-	// bad
-	foo(pc)
+	testMyDict()
 }
